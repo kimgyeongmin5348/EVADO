a0c17583d0557a964156fcb8fe9854707da18bff
MainPC <129085079+808Jade@users.noreply.github.com>
2025-03-26 00:26:53 +0900

    필요X 코드 삭제, Item 클래스 추가

---
+++ Project/Object_Items.cpp
@@ -0,0 +1,46 @@
+#include "stdafx.h"
+#include "Object_Items.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+Shovel::Shovel(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, CLoadedModelInfo* pModel)
+{
+	CLoadedModelInfo* pShovelModel = pModel;
+	if (!pShovelModel) pShovelModel = CGameObject::LoadGeometryAndAnimationFromFile(pd3dDevice, pd3dCommandList, pd3dGraphicsRootSignature, "Model/Item/Shovel.bin", NULL);
+
+	SetChild(pShovelModel->m_pModelRootObject, true);
+}
+
+Shovel::~Shovel()
+{
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+HandMap::HandMap(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, CLoadedModelInfo* pModel)
+{
+}
+
+HandMap::~HandMap()
+{
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+FlashLight::FlashLight(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, CLoadedModelInfo* pModel)
+{
+}
+
+FlashLight::~FlashLight()
+{
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+Whistle::Whistle(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, CLoadedModelInfo* pModel)
+{
+}
+
+Whistle::~Whistle()
+{
+}
