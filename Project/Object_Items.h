e380bce96cca6745d410afda482fab4de1565dd2
MainPC <129085079+808Jade@users.noreply.github.com>
2025-03-26 00:47:20 +0900

    Update Object_Items.h

---
+++ Project/Object_Items.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Object.h"
+
+class Item : public CGameObject
+{
+public:
+	Item() {};
+	virtual ~Item() {};
+};
+
+class Shovel : public Item
+{
+public:
+	Shovel(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, CLoadedModelInfo* pModel) {};
+	virtual ~Shovel();
+};
+
+class HandMap : public Item
+{
+public:
+	HandMap(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, CLoadedModelInfo* pModel);
+	virtual ~HandMap();
+};
+
+class FlashLight : public Item
+{
+public:
+	FlashLight(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, CLoadedModelInfo* pModel);
+	virtual ~FlashLight();
+};
+
+class Whistle : public Item
+{
+public:
+	Whistle(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, CLoadedModelInfo* pModel);
+	virtual ~Whistle();
+};
